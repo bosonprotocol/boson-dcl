54964e3e087859267e8c7440048522237d9281fe
Dong-Ha Kim <dongha.kim210@gmail.com>
2022-08-24 11:06:33 +0200

    feat(scene): example integration with library

--- scene/src/game.ts
+++ scene/src/game.ts
@@ -1,55 +1,77 @@
 /// --- Set up a system ---
 
-import { useBoson } from "./boson"
+import { useBoson } from "./boson";
 
 class RotatorSystem {
   // this group will contain every entity that has a Transform component
-  group = engine.getComponentGroup(Transform)
+  group = engine.getComponentGroup(Transform);
 
   update(dt: number) {
     // iterate over the entities of the group
     for (const entity of this.group.entities) {
       // get the Transform component of the entity
-      const transform = entity.getComponent(Transform)
+      const transform = entity.getComponent(Transform);
 
       // mutate the rotation
-      transform.rotate(Vector3.Up(), dt * 10)
+      transform.rotate(Vector3.Up(), dt * 10);
     }
   }
 }
 
 // Add a new instance of the system to the engine
-engine.addSystem(new RotatorSystem())
+engine.addSystem(new RotatorSystem());
 
 /// --- Spawner function ---
 
 function spawnCube(x: number, y: number, z: number) {
   // create the entity
-  const cube = new Entity()
+  const cube = new Entity();
 
   // add a transform to the entity
-  cube.addComponent(new Transform({ position: new Vector3(x, y, z) }))
+  cube.addComponent(new Transform({ position: new Vector3(x, y, z) }));
 
   // add a shape to the entity
-  cube.addComponent(new BoxShape())
+  cube.addComponent(new BoxShape());
 
   // add the entity to the engine
-  engine.addEntity(cube)
+  engine.addEntity(cube);
 
-  return cube
+  return cube;
 }
 
 /// --- Spawn a cube ---
 
-const cube = spawnCube(8, 1, 8)
+useBoson().then(async ({ coreSDK, userAccount }) => {
+  log("initialized core-sdk", coreSDK);
 
-cube.addComponent(
-  new OnPointerDown(() => {
-    cube.getComponent(Transform).scale.z *= 1.1
-    cube.getComponent(Transform).scale.x *= 0.9
+  // query valid offers from subgraph
+  const offers = await coreSDK.getOffers({
+    offersOrderBy: "createdAt",
+    offersOrderDirection: "desc",
+    offersFirst: 10,
+    offersFilter: {
+      validFromDate_lte: Math.floor(Date.now() / 1000),
+      quantityAvailable_gt: 0,
+    },
+  });
+  log("offers", offers);
 
-    spawnCube(Math.random() * 8 + 1, Math.random() * 8, Math.random() * 8 + 1)
-  })
-)
+  for (const [i, offer] of offers.entries()) {
+    const cube = spawnCube(i + 1, 1, 1);
 
-useBoson();
+    cube.addComponent(
+      new OnPointerDown(
+        async () => {
+          const txResponse = await coreSDK.commitToOffer(offer.id, {
+            buyer: userAccount,
+          });
+          log("commitToOffer - txResponse", txResponse);
+        },
+        {
+          button: ActionButton.POINTER,
+          hoverText: `Commit to offer: ${offer.id}`,
+        }
+      )
+    );
+  }
+});
