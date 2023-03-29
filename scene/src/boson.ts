05257c68232d091d3f4779cb6ad59d74e4b037ec
Ludovic Levalleux <levalleux_ludo@hotmail.com>
2023-03-29 14:32:47 +0100

    adapt config environment with regards to the network selected on Metamask

--- scene/src/boson.ts
+++ scene/src/boson.ts
@@ -2,9 +2,11 @@ import { initCoreSdk } from '@bosonprotocol/boson-dcl'
 import { getUserData, UserData } from '@decentraland/Identity'
 import { bosonConfig } from './bosonConfig'
 import * as crypto from '@dcl/crypto-scene-utils'
+import { getProvider } from '@decentraland/web3-provider'
+import { RequestManager } from 'eth-connect'
 
 // const targetEnv = 'production'
-const targetEnv = 'staging'
+// const targetEnv = 'staging'
 // const targetEnv = 'testing'
 
 async function getWalletAddress(): Promise<string> {
@@ -19,6 +21,12 @@ async function getWalletAddress(): Promise<string> {
 }
 
 export async function useBoson() {
+  const provider = await getProvider()
+  const requestManager = new RequestManager(provider)
+  const chainId = await requestManager.net_version()
+  // If user wallet is connected on Ethereum mainnet --> PRODUCTION
+  const targetEnv = chainId === '1' ? 'production' : 'staging'
+  log('Initialize BOSON on env', targetEnv)
   const userAccount: UserData = (await getUserData()) as UserData
 
   const walletAddress = userAccount?.publicKey || userAccount?.userId
